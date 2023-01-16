5318fd0ed787ee2bc533fab9aec1e0ea7535ab0b
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-01-16 13:57:33 +0500

    feat: added double linked list class

---
+++ src/data-structures/double-linked-list/double-linked-list.ts
@@ -0,0 +1,10 @@
+class DoubleLinkedList {
+  public head: Node | null;
+  public tail: Node | null;
+  public size: number;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+}

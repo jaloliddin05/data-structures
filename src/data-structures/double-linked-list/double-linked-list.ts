be7cbd6bfcf3f11149fb10e4d3f71aad099b5a84
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-01-16 14:58:41 +0500

    feat: added add function to double linked list

--- src/data-structures/double-linked-list/double-linked-list.ts
+++ src/data-structures/double-linked-list/double-linked-list.ts
@@ -1,10 +1,26 @@
+import { Node } from "./node";
 class DoubleLinkedList {
   public head: Node | null;
   public tail: Node | null;
   public size: number;
   constructor() {
     this.head = null;
-    this.tail = null;
+    this.tail = this.head;
     this.size = 0;
   }
+
+  add(data) {
+    let node = new Node(data);
+    if (this.head == null) this.head = node;
+    else {
+      if (this.tail) {
+        this.tail.next = node;
+        node.prev = this.tail;
+      }
+    }
+    this.tail = node;
+    this.size++;
+  }
 }
+
+export default DoubleLinkedList;
